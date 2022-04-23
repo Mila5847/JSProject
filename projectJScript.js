9cd3db90e74570ebd453aae80754f6ac0f9a79fd
Mila5847 <46633364+Mila5847@users.noreply.github.com>
2022-04-23 16:42:43 -0400

    shipping

--- projectJScript.js
+++ projectJScript.js
@@ -1,13 +1,13 @@
-//Bracelet 1
-  let displayDescription1 = document.getElementsByClassName("braceletsAndNecklaces")[0];
-  displayDescription1.addEventListener("mouseover", function(){
-  display("description1");
+//Bracelet/Necklace 1
+let displayDescription1 = document.getElementsByClassName("braceletsAndNecklaces")[0];
+displayDescription1.addEventListener("mouseover", function(){
+display("description1");
 }, false);
 displayDescription1.addEventListener("mouseout", function(){
-  hide("description1");
+hide("description1");
 }, false);
 
-// Bracelet 2
+// Bracelet/Necklace 2
 let displayDescription2 = document.getElementsByClassName("braceletsAndNecklaces")[1];
   displayDescription2.addEventListener("mouseover", function(){
   display("description2");
@@ -16,7 +16,7 @@ displayDescription2.addEventListener("mouseout", function(){
   hide("description2");
 }, false);
 
-// Bracelet 3
+// Bracelet/Necklace 3
 let displayDescription3 = document.getElementsByClassName("braceletsAndNecklaces")[2];
   displayDescription3.addEventListener("mouseover", function(){
   display("description3");
@@ -25,7 +25,7 @@ displayDescription3.addEventListener("mouseout", function(){
   hide("description3");
 }, false);
 
-// Functions
+// Functions for Bracelets/Necklces
 function display(id){
   let element = document.getElementById(id);
   element.style.display = "inline";
@@ -35,3 +35,57 @@ function hide(id){
   let element = document.getElementById(id);
   element.style.display = "none";
 }
+
+//---------------------------------------------------------------------------------------------------
+
+// Add item to cart
+function addItemToCart(itemName, price){
+    if(localStorage.getItem(itemName) == null){
+        let itemData = itemName + ";" + price + ";" + 1;
+        localStorage.setItem(itemName, itemData);
+        return;
+    }
+    let existingItem = localStorage.getItem(itemName);
+    let existingItemArrayData = existingItem.split(";");
+    existingItemArrayData[2]++;
+    let stringData = existingItemArrayData.join(";");
+    localStorage.setItem(itemName, stringData);
+}
+
+function loadItems(){
+    
+}
+
+
+//---------------------------------------------------------------------------------------------------
+
+// Delivery Information
+function delivery(){
+    if(document.getElementById('homeDeliver').checked) {
+        let street = document.getElementById("streetName");
+        street.value = '';
+        let city = document.getElementById("cityName");
+        city.value = '';
+        let state = document.getElementById("stateName");
+        state.value = '';
+        let zip = document.getElementById("zip"); 
+        zip.value = '';
+
+    }
+    else if(document.getElementById('storeDeliver').checked){
+    let storeCheckChecked = document.getElementById("storeDeliver");
+    let street = document.getElementById("streetName");
+    street.value = "77 Heaven-Road";
+    let city = document.getElementById("cityName");
+    city.value = "St. Heaven"
+    let state = document.getElementById("stateName");
+    state.value = "Los Angeles"
+    let zip = document.getElementById("zip");
+    zip.value = "6H7 1E7"
+    }
+}
+
+function homeDeliver(){
+
+}
+
